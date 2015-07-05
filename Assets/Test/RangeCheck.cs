313774ea2ce64e55198f29ef9d627da102212ba6
Keijiro Takahashi <keijiro@ZX.local>
2015-07-05 18:35:15 +0900

    Refactoring

---
+++ Assets/Test/RangeCheck.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class RangeCheck : MonoBehaviour
+{
+    const int iteration = 1000000;
+
+    void Start()
+    {
+        var sum = 0.0f;
+        var min = 0.0f;
+        var max = 0.0f;
+
+        for (var i = 0; i < iteration; i++)
+        {
+            var n = Perlin.Noise(
+                Random.Range(-100.0f, 100.0f),
+                Random.Range(-100.0f, 100.0f));
+            sum += n;
+            min = Mathf.Min(min, n);
+            max = Mathf.Max(max, n);
+        }
+
+        Debug.Log("(2D noise)");
+        Debug.Log("avg = " + sum / iteration);
+        Debug.Log("min = " + min);
+        Debug.Log("max = " + max);
+
+        sum = 0.0f;
+        min = 0.0f;
+        max = 0.0f;
+
+        for (var i = 0; i < iteration; i++)
+        {
+            var n = Perlin.Noise(
+                Random.Range(-100.0f, 100.0f),
+                Random.Range(-100.0f, 100.0f),
+                Random.Range(-100.0f, 100.0f));
+            sum += n;
+            min = Mathf.Min(min, n);
+            max = Mathf.Max(max, n);
+        }
+
+        Debug.Log("(3D noise)");
+        Debug.Log("avg = " + sum / iteration);
+        Debug.Log("min = " + min);
+        Debug.Log("max = " + max);
+    }
+}
