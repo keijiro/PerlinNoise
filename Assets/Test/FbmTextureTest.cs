2078a0f7fa304f730da6b7c1a9c874604d368a04
Keijiro Takahashi <keijiro@gmail.com>
2015-07-11 20:10:06 +0900

    Added a test suite.

---
+++ Assets/Test/FbmTextureTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class FbmTextureTest : MonoBehaviour
+{
+    public enum TestTarget { Noise2D, Noise3D }
+    public TestTarget target;
+
+    int size = 64;
+    Texture2D texture;
+
+    void Start()
+    {
+        texture = new Texture2D(size, size);
+        GetComponent<Renderer>().material.mainTexture = texture;
+    }
+
+    void UpdateTexture(System.Func<int, int, float> generator)
+    {
+        for (var y = 0; y < size; y++)
+        {
+            for (var x = 0; x < size; x++)
+            {
+                var n = (generator.Invoke(x, y) + 1) / 2;
+                texture.SetPixel(x, y, new Color(n, n, n));
+            }
+        }
+        texture.Apply();
+    }
+
+    void Update()
+    {
+        if (target == TestTarget.Noise2D)
+            UpdateTexture((x, y) => Perlin.Fbm(1.0f / size * x + Time.time, 1.0f / size * y, 3));
+        else
+            UpdateTexture((x, y) => Perlin.Fbm(1.0f / size * x, 1.0f / size * y, Time.time, 3));
+    }
+}
---
+++ Assets/Test/ValueRangeTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ValueRangeTest : MonoBehaviour
+{
+    const int iteration = 100 * 100 * 100;
+
+    string DoTest(System.Func<float> generator)
+    {
+        var sum = 0.0f;
+        var min = 0.0f;
+        var max = 0.0f;
+
+        for (var i = 0; i < iteration; i++)
+        {
+            var n = generator.Invoke();
+            sum += n / iteration;
+            min = Mathf.Min(min, n);
+            max = Mathf.Max(max, n);
+        }
+
+        return "avg=" + (sum / iteration) + ", min=" + min + ", max=" + max;
+    }
+
+    void Start()
+    {
+        string text = "2D Noise Test: ";
+
+        text += DoTest(() => Perlin.Noise(
+            Random.Range(-100.0f, 100.0f),
+            Random.Range(-100.0f, 100.0f)));
+
+        text += "\n3D Noise Test: ";
+
+        text += DoTest(() => Perlin.Noise(
+            Random.Range(-100.0f, 100.0f),
+            Random.Range(-100.0f, 100.0f),
+            Random.Range(-100.0f, 100.0f)));
+
+        Debug.Log(text);
+    }
+}
